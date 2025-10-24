9e9f532af953521484912c844c903dc9b84f0e67
tashun <tashun.chamara@gmail.com>
2025-10-25 02:08:37 +0530

    off chain aggregators and on chain data get using 3rd party apis framework

---
+++ oracle-service/internal/util/blockchains.go
@@ -0,0 +1,14 @@
+package util
+
+type BlockchainIds string
+
+const (
+	Ethereum          BlockchainIds = "ethereum"
+	Bitcoin           BlockchainIds = "bitcoin"
+	Solana            BlockchainIds = "solana"
+	Polygon           BlockchainIds = "polygon"
+	BinanceSmartChain BlockchainIds = "binance_smart_chain"
+	Avalanche         BlockchainIds = "avalanche"
+	Arbitrum          BlockchainIds = "arbitrum"
+	Optimism          BlockchainIds = "optimism"
+)
